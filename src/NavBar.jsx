4e52910b277a7e68aae2ede4f4da12096e8da214
Sijan Malla <sijan.malla101@gmail.com>
2023-09-16 12:18:16 -0500

    Added the dynamic height using useRef()

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,14 +1,22 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { FaBars } from 'react-icons/fa';
 import { links } from './data';
 import logo from './logo.svg';
 
 const NavBar = () => {
   const [showLinks, setShowLinks] = useState(false);
-
+  //   const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
   const toggleLinks = () => {
     setShowLinks(!showLinks);
   };
+
+  const linkStyles = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : '0px',
+  };
+
   return (
     <nav>
       <div className="nav-center">
@@ -18,19 +26,22 @@ const NavBar = () => {
             <FaBars />
           </button>
         </div>
-        {showLinks && (
-          <div className="links-container">
-            <ul className="links">
-              {links.map((link) => {
-                return (
-                  <li key={link.id}>
-                    <a href={link.url}>{link.text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
+
+        <div
+          className="links-container"
+          //   ref={linksContainerRef}
+          style={linkStyles}
+        >
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              return (
+                <li key={link.id}>
+                  <a href={link.url}>{link.text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </nav>
   );
